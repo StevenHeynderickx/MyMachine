7aee982c4a0c927fb12b506fd53eb0f93317f753
StevenHeynderickx <info@artizen.be>
2026-04-14 11:08:39 +0200

    Update LedController.h

--- LedController.h
+++ LedController.h
@@ -2,28 +2,36 @@
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include "config.h"
 
 class LedController {
 public:
 	// Constructor:
-	LedController(uint8_t NUMPIXELS,uint8_t _PIN_LEDSTRIP, NEO_GRB + NEO_KHZ800);
+	LedController();
 
 	void begin();
 	void tick();
 
 private:
 	// Mogelijke toestanden van het spel
-	/* enum State {
-		GS_OPSTARTEN,
-		GS_WAIT_FOR_GAME_SELECT,
-
+	enum LedState {
+		LS_OPSTARTEN,
+		LS_ALL_OFF,
+		LS_OFF,
+		LS_ON_COLOR,
+		LS_BLINK_COLOR,
+		LS_DROP_EFFECT
 	};
-	*/
 	// De "Globale" variabelen voor dit object:
 	// Alle methodes in deze class kunnen gebruik maken van deze
 	// "private" globale variabelen. Vandaar dat ze starten met "_".
 	// Dit is een afspraak en is niet noodzakelijk.
-	
-constexpr uint8_t _NUMPIXELS = 30;
-constexpr uint8_t _PIN_LEDSTRIP = 4;
+
+	LedState 			_state; // Dit is de LedState
+	unsigned long 		_stateStartTime; // is de millis bij aanvang van een gameState
+
+	// In deze class is er deze hulpfunctie die om debuggen en controle
+	// wat makkelijker te maken, telkens de nieuwe gameState print en 
+	// de State-timer reset.
+	void setLedState(LedState newState);
 };
